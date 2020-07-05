5edf7240097e3cf91e995c8d73c8620985a55b76
ludwig Flores <louiejf1@gmail.com>
2020-07-05 13:02:16 -0700

    Udpated layout

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -6,24 +6,41 @@ import Button from "../Components/Button";
 export default function Home() {
   return (
     <div className="MainHomeDiv">
-        <Jumbotron />
-      <div className="container-fluid">
-        <div><Button /></div>
+      <Jumbotron />
+      <div className="container-fluid register">
         <div>
-            Contents section <br></br>
-            Contents section <br></br>
-            Contents section <br></br>
-            Contents section <br></br>
-            Contents section <br></br>
-            Contents section <br></br>
-            Contents section <br></br>
-            Contents section <br></br>
-            Contents section <br></br>
-            Contents section <br></br>
-            Contents section <br></br>
-            Contents section <br></br>
-            Contents section <br></br>
-            Contents section <br></br>
+        </div>
+        <div>
+          <div class="row">
+            <div class="col-sm-6">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Sign in</h5>
+                  <p class="card-text">
+                    With supporting text below as a natural lead-in to
+                    additional content.
+                  </p>
+                  <a href="#" class="btn btn-primary">
+                    Go somewhere
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Register</h5>
+                  <p class="card-text">
+                    With supporting text below as a natural lead-in to
+                    additional content.
+                  </p>
+                  <a href="#" class="btn btn-primary">
+                    Go somewhere
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <Footer />
