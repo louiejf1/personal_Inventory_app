87582af55c3044515f00ae04d5fe27f4a802c462
washiPolly <pollylsu@gmail.com>
2020-07-08 21:28:43 -0700

    added closet page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home";
+import Closet from "./Pages/Closet";
 import Collectables from "./Pages/Collectables";
 import Paperwork from "./Pages/Paperwork";
 import Register from "./Pages/Register";
@@ -16,6 +17,7 @@ class App extends Component {
           <Router>
             <Switch>
               <Route exact path="/" component={Home} />
+              <Route path="/closet" component={Closet} />
               <Route path="/collectables" component={Collectables} />
               <Route path="/paperwork" component={Paperwork} />
               <Route path="/register" component={Register} />
