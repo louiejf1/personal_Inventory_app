1216e43dfeebfdf67d8275d0f6a8c1ce8d9bdeac
ludwig Flores <louiejf1@gmail.com>
2020-07-11 13:21:37 -0700

    Update as discussed Saturday

--- controllers/inventoryItemsController.js
+++ controllers/inventoryItemsController.js
@@ -1,34 +1,34 @@
-const db = require("../models");
+const { inventoryItems } = require("../models");
 
-// Defining methods for the inventoryItemssController
+// Defining methods for the inventoryItemsController
 module.exports = {
   findAll: function(req, res) {
-    db.inventoryItems
+    inventoryItems
       .find(req.query)
       .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
-    db.inventoryItems
+    inventoryItems
       .findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
-    db.inventoryItems
+    inventoryItems
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    db.inventoryItems
+    inventoryItems
       .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
-    db.inventoryItems
+    inventoryItems
       .findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
