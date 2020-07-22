3e1831a485e013b040483d8ab0e811a57ab83bc0
umaiibo <j.hnpn123@gmail.com>
2020-07-22 15:07:39 -0700

    added input area for search component

--- client/src/Components/Search.js
+++ client/src/Components/Search.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState, Component, useEffect } from 'react';
 
-export default function Search() {
-    return (
-        <div>
-            
-        </div>
-    )
+export class Search extends Component {
+
+    render() {
+        return (
+            <div>
+                <input
+                    type="search"
+                    name="title"
+                    style={{ flex: '10', padding: '5px' }}
+                    placeholder="Search for an item..."
+                    value={this.state.title}
+                    onChange={this.onChange}
+                />
+            </div>
+        )
+    }
 }
+
+export default Search
