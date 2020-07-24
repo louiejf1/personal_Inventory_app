16a490c15293ea688f3be59fa9b7a5af31c61093
Chris Recinos <recinos.c.156@gmail.com>
2020-07-24 12:07:53 -0700

    updates to routes

--- models/inventoryItems.js
+++ models/inventoryItems.js
@@ -16,48 +16,10 @@ const inventoryItemsSchema = new Schema({
     important: { type: Boolean, default: false },
 
     date: { type: Date, default: Date.now }
-<<<<<<< HEAD
   }
 
 });
 
-=======
-});
-
-const collectiblesSchema = new Schema({
-    invCategory: { type: String, required: true },
-    invItemName: { type: String, required: true },
-    invItemDescription: String,
-
-    invItemImgUrl: String,
-
-    important: { type: Boolean, default: false },
-
-    date: { type: Date, default: Date.now }
-});
-
-const paperworkSchema = new Schema({
-    invCategory: { type: String, required: true },
-    invItemName: { type: String, required: true },
-
-    important: { type: Boolean, default: false },
-    invItemImgUrl: String,
-
-    invItemDescription: String,
-    important: { type: Boolean, default: false },
-
-    date: { type: Date, default: Date.now }
-});
-
-const donationsSchema = new Schema({
-    invCategory: { type: String, required: true },
-    invItemName: { type: String, required: true },
-    invItemDescription: String,
-
-    invItemImgUrl: String,
-
-    important: { type: Boolean, default: false },
->>>>>>> 47b2db553310320352d94a073a22f4967ad4a1fa
 
 
 const inventoryItems = mongoose.model("inventoryItems", inventoryItemsSchema);
